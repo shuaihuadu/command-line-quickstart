3082bf07e9604f7521c70c894b882b0e03161a44
ShuaiHua Du <shuaihua.du@outlook.com>
2023-07-24 13:21:03 +0800

    Init commit.

---
+++ QuickStart.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿var fileOption = new Option<FileInfo?>(
+    name: "--file",
+    description: "The file to read and display on the console."
+    );
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
+
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) =>
+{
+    ReadFile(file!);
+}, fileOption);
+
+await rootCommand.InvokeAsync(args);
+
+static void ReadFile(FileInfo file)
+{
+    File.ReadLines(file.FullName, Encoding.UTF8).ToList()
+        .ForEach(line => Console.WriteLine(line));
+}
